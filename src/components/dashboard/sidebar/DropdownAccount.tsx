a142e96713024e27159d52f30f07b455d87522ea
Saddam Hossain <sksaddamh08@gmail.com>
2024-05-18 19:00:27 +0530

    chore: Update AccountSettings component to accept user prop

--- src/components/dashboard/sidebar/DropdownAccount.tsx
+++ src/components/dashboard/sidebar/DropdownAccount.tsx
@@ -54,7 +54,7 @@ const DropdownAccount = async () => {
         </div>
         <DropdownMenuSeparator />
 
-        <AccountSettings />
+        <AccountSettings user={user!} />
 
         <DropdownMenuSeparator />
 
