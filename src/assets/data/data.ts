aaff679708a21b7f15f1beab6efc2d58e47d34ab
mselbekk11 <mselbekk11@gmail.com>
2024-10-31 09:57:14 -0700

    updates

--- src/assets/data/data.ts
+++ src/assets/data/data.ts
@@ -1,23 +1,44 @@
 export const testimonials = [
   {
     image: '/luba.png',
+    name: 'Luba',
+    quote: 'One',
+    title: 'One',
   },
   {
     image: '/dan.png',
+    name: 'Dan',
+    quote: 'Two',
+    title: 'Two',
   },
   {
     image: '/luba-queen.png',
+    name: 'LubaTwo',
+    quote: 'Three',
+    title: 'Three',
   },
   {
     image: '/morgan.png',
+    name: 'Morgan',
+    quote: 'Four',
+    title: 'Four',
   },
   {
     image: '/viking.png',
+    name: 'Viking',
+    quote: 'Five',
+    title: 'Five',
   },
   {
     image: '/luba2.png',
+    name: 'LubaThree',
+    quote: 'Six',
+    title: 'Six',
   },
   {
     image: '/morgan2.png',
+    name: 'MorganTwo',
+    quote: 'Seven',
+    title: 'Seven',
   },
 ];
