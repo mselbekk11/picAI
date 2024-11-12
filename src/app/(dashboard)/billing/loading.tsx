05798fbe0c826ea111e779b8f8c458aeffe80f7e
mselbekk11 <mselbekk11@gmail.com>
2024-11-11 19:46:28 -0800

    updates

--- src/app/(dashboard)/billing/loading.tsx
+++ src/app/(dashboard)/billing/loading.tsx
@@ -4,19 +4,19 @@ export default function LoadingPage() {
   return (
     <div className='flex flex-col p-6'>
       <Skeleton className='rounded-md p-6'>
-        <div className='mb-8'>
+        <div className='mb-6'>
           <Skeleton className='h-[10px] w-[100px] rounded-md mb-2'></Skeleton>
           <Skeleton className='h-[20px] w-[200px] rounded-md'></Skeleton>
         </div>
-        <div className='mb-8'>
+        <div className='mb-6'>
           <Skeleton className='h-[10px] w-[100px] rounded-md mb-2'></Skeleton>
           <Skeleton className='h-[20px] w-[200px] rounded-md'></Skeleton>
         </div>
-        <div className='mb-8'>
+        <div className='mb-6'>
           <Skeleton className='h-[10px] w-[100px] rounded-md mb-2'></Skeleton>
           <Skeleton className='h-[20px] w-[200px] rounded-md'></Skeleton>
         </div>
-        <div className='mb-8'>
+        <div className='mb-6'>
           <Skeleton className='h-[10px] w-[100px] rounded-md mb-2'></Skeleton>
           <Skeleton className='h-[20px] w-[200px] rounded-md'></Skeleton>
         </div>
